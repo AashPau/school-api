6339951fe617dd89aa8832bba2a020d05c8e90aa
Aash <31497659+AashPau@users.noreply.github.com>
2024-07-01 21:02:44 +1000

    add validation for new users

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,13 @@
 import express from "express";
+import { newUserValidation } from "../middlewares/joiValidation";
 
 const router = express.Router();
 
-export const insertNewUser = (req, res, next) => {
+router.post("/", newUserValidation, async (req, res, next) => {
   try {
   } catch (error) {
     next(error);
   }
-};
+});
 
 export default router;
