14e9e6853b7ad5ddfa3a98f77bea8b79d5b3ef5e
Aash <31497659+AashPau@users.noreply.github.com>
2024-07-03 13:12:00 +1000

    added get method for student router

--- src/routers/studentRouter.js
+++ src/routers/studentRouter.js
@@ -2,15 +2,26 @@ import express from "express";
 import { newUserValidation } from "../middlewares/joiValidation.js";
 import { insertStudent } from "../model/student/studentModel.js";
 import { hashPassword } from "../utils/bcrypt.js";
+import { auth } from "../middlewares/auth.js";
 
 const router = express.Router();
 
-router.get("/", auth, (req, res, next)=>{
+router.get("/", auth, (req, res, next) => {
   try {
-    const {userInfo}
+    const { userInfo } = req;
+    userInfo?.refreshJWT = undefined;
+
+    userInfo?.status === "active"?res.json({
+      status:"success",
+      message:"",
+      userInfo,
+    }):res.json({
+      stauts:"error",
+      message:"your account had not been activated please contact admin."
+    })
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 export default router;
